b3e9c39853f240d198ab6d1b2c5af456a2ffa35b
easy-young <leejy230698@naver.com>
2022-05-16 09:27:57 +0900

    default setting

---
+++ back/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+const COOKIE_SECRET = process.env.COOKIE_SECRET || 'donut';
+
+const userRouter = require('./routes/user');
+const commentRouter = require('./routes/comment');
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser(COOKIE_SECRET));
+
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+}));
+
+app.use('/user', userRouter);
+app.use('/comment', commentRouter);
+
+app.listen(PORT, ()=>{
+    console.log('front server 3000');
+});
