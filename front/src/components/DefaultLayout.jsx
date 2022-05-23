cbf17f0d898d610bdb07d3cdd7489dc7bd87b374
bbb4756 <rlarbfl4756@gmail.com>
2022-05-23 16:37:56 +0900

    메뉴바수정

--- front/src/components/DefaultLayout.jsx
+++ front/src/components/DefaultLayout.jsx
@@ -1,12 +1,18 @@
-import { useCallback } from 'react';
+import { useCallback, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { Menu, Layout } from 'antd';
 import { show, hidden } from '../reducers/display.js';
 import styled from 'styled-components';
+import { keyframes } from 'styled-components';
 import Sider from 'antd/lib/layout/Sider';
 import SFLemon from '../font/fonts';
 
+const Header = styled.header`
+    width:100%;
+    height:10vh;
+    
+`
 const StyledSider = styled(Sider)`
     position: fixed;
     z-index: 1000;
@@ -19,7 +25,6 @@ const StyledMenu = styled(Menu)`
     font-family: 'YdestreetB';
     font-size:20px;
 
-
 `
 
 const StyledButton = styled.button`
@@ -29,28 +34,66 @@ const StyledButton = styled.button`
     border: none;
     :hover {
         cursor: pointer;
+        color: red;
     }
 
 `;
+const dungdung = keyframes`
+
+    0% {
+        margin-top: 0.5%
+    }
+    25%{
+        margin-top:0.2%
+    }
+
+    50%{
+        margin-top:0.5%
+    }
+    75%{
+        margin-top: -0.2%
+    }
+    100%{
+        margin-top: 0.5%
+    }
+
+`
+
 
 const Img = styled.img`
-    display: fixed;
     float: right;
     margin: 4px 12px 0 0;
+    animation: ${dungdung} 2.0s linear infinite;
+    cursor:pointer;
+    transition: all 2s;
+    :hover{
+        transition: all ease 2s 0s;
+    }
 `;
+
+
 
 const DefaultHeader = () => {
     const dispatch = useDispatch();
     const header = useSelector((state) => state.display);
     const onShow = useCallback(() => { dispatch(show()) }, [dispatch]);
     const onHidden = useCallback(() => { dispatch(hidden()) }, [dispatch]);
+    const menuMouseOver = (e)=>{
+        console.log(e.target.style)
+        e.target.src = "http://localhost:3000/img/donut_set_hover.png"
+    }
+
+    const menuMouseOut = (e)=>{
+        e.target.src = "http://localhost:3000/img/donut_set.png"
+    }
 
     return (
         <>
-            <header>
+            <Header>
                 <Link to="/">도넛철도 999</Link>
-                <Img onClick={onShow} src="http://localhost:3000/menu.png" width={40} height={40} alt=''/>
-            </header>
+                <Img onClick={onShow} src="http://localhost:3000/img/donut_set.png" width={100} height={80} alt=''
+                onMouseOver={menuMouseOver} onMouseOut={menuMouseOut}/>
+            </Header>
             {
                 header.display === 'block' &&
                 <Layout>
@@ -62,18 +105,18 @@ const DefaultHeader = () => {
                             style={{ height: '100vh' }}
                         >
                             <StyledButton onClick={onHidden}>X</StyledButton>
-                            <StyledMenu.Item key='0'><Link to="/mypage">슿 마이 페이지</Link></StyledMenu.Item>
+                            <StyledMenu.Item key='0' style={{}}><Link to="/mypage">슿 마이 페이지</Link></StyledMenu.Item>
                             <StyledMenu.Item key='1'><Link to="/login">쳌 로그인</Link></StyledMenu.Item>
                             <StyledMenu.Item key='2'><Link to="/rank"> 👑 랭킹</Link></StyledMenu.Item>
                             <StyledMenu.SubMenu key='3' title='슾 테마'>
-                                <StyledMenu.Item key='protein'><Link to="/theme/protein">프로틴 도넛</Link></StyledMenu.Item>
-                                <StyledMenu.Item key='photo'><Link to="/theme/photo">포토존</Link></StyledMenu.Item>
-                                <StyledMenu.Item key='unique'><Link to="/theme/unique">이색 도넛</Link></StyledMenu.Item>
-                                <StyledMenu.Item key='parking'><Link to="/theme/parking">주차 가능</Link></StyledMenu.Item>
+                                <StyledMenu.Item key='protein' style={{fontSize:"16px"}}><Link to="/theme/protein">프로틴 도넛</Link></StyledMenu.Item>
+                                <StyledMenu.Item key='photo' style={{fontSize:"16px"}}><Link to="/theme/photo">포토존</Link></StyledMenu.Item>
+                                <StyledMenu.Item key='unique' style={{fontSize:"16px"}}><Link to="/theme/unique">이색 도넛</Link></StyledMenu.Item>
+                                <StyledMenu.Item key='parking' style={{fontSize:"16px"}}><Link to="/theme/parking">주차 가능</Link></StyledMenu.Item>
                             </StyledMenu.SubMenu>
                             <StyledMenu.SubMenu key='4' title='늇 맛집 등록'>
-                                <StyledMenu.Item key='join'><Link to="/register/join">등록 신청</Link></StyledMenu.Item>
-                                <StyledMenu.Item key='check'><Link to="/register/check">등록 확인</Link></StyledMenu.Item>
+                                <StyledMenu.Item key='join' style={{fontSize:"16px"}}><Link to="/register/join">등록 신청</Link></StyledMenu.Item>
+                                <StyledMenu.Item key='check' style={{fontSize:"16px"}}><Link to="/register/check">등록 확인</Link></StyledMenu.Item>
                             </StyledMenu.SubMenu>
                         </StyledMenu>
                     </StyledSider>
