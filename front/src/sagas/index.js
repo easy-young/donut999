c6782d615c4b0f612b65bd1b25004226b65391f9
easy-young <leejy230698@naver.com>
2022-05-26 14:16:12 +0900

    conflict 해결2

--- front/src/sagas/index.js
+++ front/src/sagas/index.js
@@ -55,7 +55,7 @@ export default function* rootSaga() {
         updateReviewSaga(),
         stationSaga(),
         shopSaga(),
-        rankSaga()
+        rankSaga(),
         themeSaga()
     ]);
     
