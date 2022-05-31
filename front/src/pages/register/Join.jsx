75cc05aa72362090bd441b1e135969353f0bd1a4
o-haa <haeun9627@hanmail.net>
2022-05-31 21:22:25 +0900

    admin css 합침

--- front/src/pages/register/Join.jsx
+++ front/src/pages/register/Join.jsx
@@ -1,33 +1,17 @@
 import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import styled, {keyframes} from 'styled-components';
+import styled from 'styled-components';
+import { Background, BottomDiv, BackBtn, Submit } from '../../components/styles/AdminStyles';
 import { register_request, register_gohome, register_name, register_email } from '../../reducers/register';
 import SFLemon from '../../font/fonts';
 
-const Background = styled.div`
-    display: flex;
-    position: fixed;
-    z-index: 2000;
-    width: 100%;
-    height: 100%;
-    justify-content: center;
-    top: 0px;
-    align-items: center;
-    background: rgba(0, 0, 0, 0.4);
-`;
 
 const H1 = styled.h1`
     text-align: center;
     text-shadow: 0 0 7px #f3d8ff, 0 0 10px #f3d8ff, 0 0 21px #EE82EE, 0 0 20px #EE82EE,
     0 0 30px #EE82EE, 0 0 20px #EE82EE, 0 0 30px #EE82EE, 0 0 30px #EE82EE;
 `;
-
-const H3 = styled.h3`
-    margin-top: 20px;
-`;
-
-
 
 
 const Form = styled.form`
@@ -61,66 +45,6 @@ const Input = styled.input`
 
     @media (max-width: 300px) {
         width: 22%;
-    }
-`;
-
-const BottomDiv = styled.div`
-    width:100%;
-    display: flex;
-    justify-content: center;
-    margin-top: 26px;
-`;
-
-const motion = keyframes`
- 
-	0% {margin-top: 0px;} 
-	100% {margin-top: 5px;}
-
-`
-
-const Submit = styled.input`
-    width: 10%;
-    height: 8%;
-    margin-right: 20px;
-    padding: 5px 7px;
-    background: #EE82EE;
-    color:#fff;
-    border-color:pink;
-
-
-    cursor: pointer;
-    &:hover{
-        opacity: 0.5;
-        animation: ${motion} 0.3s linear 0s infinite alternate; 
-    }
-
-    @media (max-width: 600px) {
-        width: 15%;
-        animation: ${motion} 0.3s linear 0s infinite alternate; 
-
-    }
-`;
-
-
-
-
-const Back = styled.button`
-    width:20%;
-    height: 8%;
-    padding: 5px 7px;
-    background: #fff;
-
-    cursor: pointer;
-    &:hover{
-        opacity: 0.5;
-    }
-
-    &>a{
-        color:#000;
-    }
-
-    @media (max-width: 600px) {
-        width: 30%;
     }
 `;
 
@@ -200,29 +124,29 @@ const Join = () => {
                     <H1 style={{color:'#fff'}}> 
                          스토어 등록 신청 <span>💫</span>
                     </H1>
-                    <H3>🏡 가게명</H3>
+                    <h3 style={{ marginTop: '20px'}}>🏡 가게명 </h3>
                     <Input type='text' style={{ width: '100%' }} name='name'/>
                     {
                         name &&
                         <span>가게명을 입력해 주세요.</span>
                     }
-                    <H3>🍩 대표 도넛 (최대 3개)</H3>
+                    <h3 style={{ marginTop: '20px'}}>🍩 대표 도넛 (최대 3개)</h3>
                     <Input type='text' style={{ marginRight: '16px' }} name='menu1' />
                     <Input type='text' style={{ marginRight: '16px' }} name='menu2' />
                     <Input type='text' name='menu3' />
-                    <H3>📮 주소</H3>
+                    <h3 style={{ marginTop: '20px'}}>📮 주소</h3>
                     <Input type='text' style={{ width: '100%' }} name='address' placeholder='서울 지역만 가능합니다.'/>
-                    <H3>📧 신청자 이메일</H3>
+                    <h3 style={{ marginTop: '20px'}}>📧 신청자 이메일</h3>
                     <Input type='text' style={{ width: '100%' }} name='email' placeholder='등록 승인 결과를 이메일로 알려드립니다.'/>
                     {
                         email &&
                         <span>이메일을 입력해 주세요.</span>
                     }
-                    <H3>👥 SNS 계정 (선택)</H3>
+                    <h3 style={{ marginTop: '20px'}}>👥 SNS 계정 (선택)</h3>
                     <Input type='text' style={{ width: '100%' }} name='sns' />
                     <BottomDiv>
                         <Submit type='submit' value='신청' />
-                        <Back><Link to='/'>뒤로 가기</Link></Back>
+                        <BackBtn><Link to='/'>뒤로 가기</Link></BackBtn>
                     </BottomDiv>
                     
                 </Form>
